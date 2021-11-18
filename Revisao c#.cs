5c8cbdc2d5b06afe0003f8587dbf302cf06437be
DANI <93198583+Danisouzax@users.noreply.github.com>
2021-11-18 11:07:11 -0300

    Add files via upload

---
+++ Revisao c#.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Revisao_C
+{
+    class Program
+    {
+        private static int indiceAluno;
+
+        static void Main(string[] args)
+        {
+            Aluno[] alunos = new Aluno[5];
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        Console.WriteLine("Informe o nome do aluno:");
+                        var aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+
+                        Console.WriteLine("Informe a nota do aluno:");
+
+                        if (decimal.TryParse(Console.ReadLine(), out decimal nota))
+                        {
+                            aluno.Nota = nota;
+                        }
+                        else
+                        {
+                            throw new ArgumentException("Valor da nota deve ser decimal");
+                        }
+
+                        alunos[indiceAluno] = aluno;
+                        indiceAluno++;
+
+                        break;
+                    case "2":
+                        foreach (var a in alunos)
+                        {
+                            if (!string.IsNullOrEmpty(a.Nome))
+                            {
+                                Console.WriteLine($"ALUNO: {a.Nome} - NOTA: {a.Nota}");
+                            }
+                        }
+                        break;
+                    case "3":
+
+                        break;
+                    default:
+                        throw new ArgumentException();
+                }
+
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine("1. Inserir novo aluno:");
+            Console.WriteLine("2. Listar alunos");
+            Console.WriteLine("3. Calcular média geral");
+            Console.WriteLine("X. Sair");
+            Console.WriteLine();
+
+            String opcaoUsuario = Console.ReadLine();
+            Console.WriteLine();
+            return opcaoUsuario;
+        }
+    }
+}
